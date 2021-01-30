b735628c7131bea092dd830e43ec015d3a0820c0
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-01-31 00:49:51 +0200

    added local storage theme state

--- src/components/ThemeSwitch/index.tsx
+++ src/components/ThemeSwitch/index.tsx
@@ -8,10 +8,30 @@ import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 export class ThemeSwitch extends Component {
 
-  private darkTheme = false;
+  state = {
+    darkTheme: false
+  }
+
+  componentDidMount() {
+    this.setState({
+      darkTheme: localStorage.getItem('theme') === 'dark'
+    });
+    if (localStorage.getItem('theme') === 'dark') {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    }
+  }
 
   onToggle(e: React.FormEvent<HTMLInputElement>) {
-    console.log(e.currentTarget.checked);
+    this.setState({
+      darkTheme: e.currentTarget.checked
+    });
+    if (e.currentTarget.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
   }
 
   render() {
@@ -19,8 +39,16 @@ export class ThemeSwitch extends Component {
       <div className="app-container__theme-switch">
         <h4 className="theme-switch__heading">Light Mode</h4>
         <div className="theme-switch__toggler">
-          {this.darkTheme && <FontAwesomeIcon icon={faSun} />}
-          <input onChange={(e) => this.onToggle(e)} type="checkbox" className="toggler__checkbox" id="checkbox" name="checkbox" />
+          {this.state.darkTheme ?
+            <FontAwesomeIcon icon={faMoon} className="toggler__icon" /> :
+            <FontAwesomeIcon icon={faSun} className="toggler__icon" />}
+          <input
+            onChange={(e) => this.onToggle(e)}
+            type="checkbox"
+            className="toggler__checkbox"
+            id="checkbox"
+            name="checkbox"
+            checked={this.state.darkTheme} />
           <label htmlFor="checkbox" className="toggler__slider"></label>
         </div>
       </div>
